a95307da4ebb1a74d61f4e22be28bc004ea6f9e8
Joe Santoru <joesantoru@gmail.com>
2017-08-20 15:45:52 -0400

    added intrinio user/pass config

--- conf/config.js
+++ conf/config.js
@@ -1,6 +1,10 @@
 var apikeyAlphavantage = require('./apikey-alphavantage');
+var userpassIntrinio = require('./userpass-intrinio');
 
 var config = {};
 config.apikeyAlphavantage = apikeyAlphavantage;
+
+config.intrinioUser = userpassIntrinio.user;
+config.intrinioPass = userpassIntrinio.pass;
 
 module.exports = config;
