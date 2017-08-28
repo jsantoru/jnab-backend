ef630a1d1d5d6ef9c67ce36bd7284bfa5be178ad
Joe Santoru <joesantoru@gmail.com>
2017-08-27 20:57:02 -0400

    small change to initialize config

--- conf/config.js
+++ conf/config.js
@@ -1,17 +1,16 @@
 var privateConfig = require('./private-config');
 
-var config = {};
-config.app = {
-    apikey: privateConfig.app.apikey
-};
-
-config.alphavantage = {
-    apikey: privateConfig.alphavantage.apikey
-};
-
-config.intrinio = {
-    user: privateConfig.intrinio.user,
-    pass: privateConfig.intrinio.pass
+var config = {
+    app: {
+        apikey: privateConfig.app.apikey
+    },
+    alphavantage: {
+        apikey: privateConfig.alphavantage.apikey
+    },
+    intrinio: {
+        user: privateConfig.intrinio.user,
+        pass: privateConfig.intrinio.pass
+    }
 };
 
 module.exports = config;
