53bf07d1b6fb6733cc6779aba06bbf7f8de5cb04
Joe Santoru <joesantoru@gmail.com>
2017-09-03 16:33:35 -0400

    setup to serve the frontend at /jnab/ and api at /api

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ var express = require('express');
 var cors = require('cors');
 var routes = require('./routes');
 var config = require('../conf/config');
+var greenlock = require('greenlock-express');
 
 // init the app and set the routes
 var app = express();
 app.use(cors());
+
+// serve the frontend
+app.use('/jnab', express.static('public'));
 
 // apikey is required for all calls
 app.use(function(req, res, next) {
@@ -25,10 +29,19 @@ app.use(function(req, res, next) {
 });
 
 // use the routes defined in the router
-app.use('/', routes.router);
+app.use('/api/', routes.router);
 
 // TODO: use https
 // start the app server
-app.listen(8080, function () {
-	console.log('listening on port 8080...');
+app.listen(6001, function () {
+	console.log('listening on port 6001...');
 });
+
+/*greenlock.create({
+    server: 'staging',
+    email: 'john.doe@example.com',
+    agreeTos: true,
+    approveDomains: ['example.com'],
+    app: app
+}).listen(6001);
+*/
